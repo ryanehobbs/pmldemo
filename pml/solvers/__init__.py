d4b6e74c50b42b2816e0292e739bfad623399f01
hobbsr <ryan.hobbs@emc.com>
2017-02-20 19:56:10 -0800

    WIP adding more comments and making code clearer to read

--- pml/solvers/__init__.py
+++ pml/solvers/__init__.py
@@ -11,7 +11,6 @@ def _sumsq(X):
 
     return np.sum(np.power(X, 2))
 
-
 def fminfunc(costfunc, X, y, initial_theta, **kwargs):
     """
 
@@ -107,7 +106,7 @@ def fminfunc(costfunc, X, y, initial_theta, **kwargs):
             # use the double dog leg optimization s^k = alpha_s1^k = alpha_s2^k,
             # where s1 = ascent search direction, s2 = quasi-Newton search direction
             # subtract because we are stepping down
-            s = -_doglegm(hesr, grad, dbldog, delta)
+            s = -doglegm(hesr, grad, dbldog, delta)
             s_norm = np.linalg.norm(dbldog * s)
 
             # initial loop get the delta based on minimum between
@@ -162,7 +161,7 @@ def fminfunc(costfunc, X, y, initial_theta, **kwargs):
     theta_cpy = np.reshape(theta_cpy, theta_sz)
     return theta_cpy, cost_outer
 
-def _doglegm(r, g, d, delta):
+def doglegm(r, g, d, delta):
     """
     The double-dogleg optimization method combines the ideas of the quasi-Newton
     and trust region methods. In each iteration, the double-dogleg algorithm computes
@@ -222,71 +221,44 @@ def _doglegm(r, g, d, delta):
 
     return x
 
-def doglegm(r, g,d, delta):
-    """
-
-    :param r:
-    :param g:
-    :param d:
-    :param delta:
-    :return:
-    """
-
-    # https://github.com/dkogan/libdogleg/blob/master/dogleg.c
-    # http://support.sas.com/documentation/cdl/en/etsug/60372/HTML/default/viewer.htm#etsug_nlomet_sect006.htm
-
-    b = np.linalg.solve(r.T, g)
-    x = np.linalg.solve(r, b)
-    xn = np.linalg.norm(np.multiply(d, x))
-
-    if (xn > delta):
-        # GN too big must scale
-        s = np.divide(g, d)
-        sn = np.linalg.norm(s)
-        if (sn > 0):
-            # normalize and rescale
-            s = np.divide((s / sn), d)
-            # get line minimzer in s direction
-            tn = np.linalg.norm(r*s)
-            snm = (sn / tn) / tn
-            if (snm < delta):
-                # get the dogleg path minimizer
-                bn = np.linalg.norm(b)
-                dxn = delta / xn
-                snmd = snm / delta
-                t = (bn / sn) * (bn / xn) * snmd
-                t -= dxn * np.power(snmd, 2) - np.sqrt(np.power((t-dxn), 2) + (1 - np.power(dxn, 2)) * (1 - np.power(snmd, 2)))
-                alpha = dxn * (1 - np.power(snmd, 2)) / t
-            else:
-                alpha = 0
-        else:
-            alpha = delta / xn
-            snm = 0;
-        # form convex combination
-        x = alpha * x + ((1 - alpha) * min(snm, delta)) * s
-    return x
-
 # https://en.wikipedia.org/wiki/Cholesky_decomposition
-def cholupdate(R,x,sign):
+def cholupdate(R, x, sign):
+    """
+    Update a Cholesky factorization. This will perform a Rank 1
+    update if sign is positive or a Rank 1 downdate if sign is negative.
+
+    Update the upper right triangle portion of the Cholesky factorized
+    matrix.
+
+    :param R: Original Cholesky factorization matrix
+    :param x: Column vector of appropriate length to add or subtract
+    :param sign: + implies (R + x * x') - implies (R - x * x')
+    :return: Updated Cholesky factorized matrix
+    """
+
+    # get length of vector X
     p = np.size(x)
-
-    #x = x.T
-
+    # iterate up to length of vector X
     for k in range(p):
-        if sign == '+':
-            inner = np.sum(np.power(R[k,k], 2) + (np.sum(np.power(x[k], 2))))
+        if sign == '+':  # rank 1 update
+            inner = np.sum(np.power(R[k, k], 2) + (np.sum(np.power(x[k], 2))))
             r = np.sqrt(inner)
-        elif sign == '-':
-            inner = np.sum(np.power(R[k,k], 2) - (np.sum(np.power(x[k], 2))))
+        elif sign == '-':  # rank 2 downdate
+            inner = np.sum(np.power(R[k, k], 2) - (np.sum(np.power(x[k], 2))))
             r = np.sqrt(inner)
-        #c = r/R[k,k]
-        c = np.divide(r, R[k,k])
-        #s = np.sum(x[k]/R[k,k])
-        s = np.sum(np.divide(x[k], R[k,k]))
-        R[k,k] = r
-        if sign == '+':
-            R[k,k+1:p] = (R[k,k+1:p] + s * x[k+1:p].T)/c
-        elif sign == '-':
-            R[k,k+1:p] = (R[k,k+1:p] - s * x[k+1:p].T)/c
-        x[k+1:p]= c * x[k+1:p] - s * R[k, k+1:p].T
+
+        # calculate outer product and update R matrix
+        c = np.divide(r, R[k, k])
+        s = np.sum(np.divide(x[k], R[k, k]))
+        R[k, k] = r
+
+        # based on sign update upper right triangle of matrix in place
+        if sign == '+':  # rank 1 update
+            R[k, k + 1:p] = (R[k, k + 1:p] + s * x[k + 1:p].T)/c
+        elif sign == '-':  # rank 2 downdate
+            R[k, k + 1:p] = (R[k, k + 1:p] - s * x[k + 1:p].T)/c
+
+        # update vector x in place
+        x[k + 1:p] = c * x[k + 1:p] - s * R[k, k + 1:p].T
+
     return R
