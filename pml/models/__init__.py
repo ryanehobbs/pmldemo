Fixed bug in fminfun and logistic cost function where gradient was not being calculated correctly.  Updated grad implementation in cost to use a vectorized implementation

pml/models/__init__.py
@@ -14,6 +14,8 @@ class LinearBase(six.with_metaclass(ABCMeta)):
     __metaclass__ = ABCMeta
 
     fitted = False
+    _X = []
+    _y = []
     def __init__(self, normalize=False, solver=None, **kwargs):
         """
         Base class for linear model regression calculations
@@ -40,6 +42,26 @@ class LinearBase(six.with_metaclass(ABCMeta)):
     def __str__(self):
 
         return self.solver
+
+    @property
+    def X_data(self):
+
+        return self._X
+
+    @X_data.setter
+    def X_data(self, value):
+
+        self._X = value
+
+    @property
+    def y_data(self):
+
+        return self._y
+
+    @y_data.setter
+    def y_data(self, value):
+
+        self._y = value
 
     @abstractmethod
     def predict(self, X):
@@ -205,7 +227,6 @@ class LinearMixin(LinearBase):
         :return:
         """
 
-        blah = np.atleast_1d(initial_theta).ndim
         if initial_theta is not None and np.atleast_1d(initial_theta).ndim < 1:
             raise ValueError("Sample weights must be 1D array or scalar")
 
@@ -219,7 +240,7 @@ class LinearMixin(LinearBase):
 
         for i in range(0, num_of_labels):
             y_idx = i + 1
-            theta, _, _ = fmincg(self.cost, X, (y == y_idx),
+            theta, _, _ = fmincg(self.cost, X, (y == i),
                                      initial_theta=initial_theta,
                                      alpha=alpha,
                                      max_iter=iterations)

pml/models/linear.py
@@ -34,6 +34,8 @@ def fitdata(func):
 
         if not klass.fitted or refit:  # call base linear class _pre_fit method
             xargs[1], xargs[2] = klass._pre_fit(X, y, theta)
+            klass.X_data = xargs[1]
+            klass.y_data = xargs[2]
 
         return func(*xargs, **kwargs)
 
@@ -196,6 +198,7 @@ class Logistic(LinearMixin):
         if not isinstance(theta, np.ndarray) and theta is not None:
             theta = np.array(theta, dtype='f')[:, None]
 
+        grad = np.zeros((theta.shape[0], 1))
         # get number of training samples
         n_samples = y.shape[0]
         # fit intercept for linear equation this is the hypothesis
@@ -204,10 +207,9 @@ class Logistic(LinearMixin):
         j_cost = (1/n_samples) * np.sum(np.multiply(-y, np.log(hX)) - np.multiply((1-y), np.log(1-hX))) + \
                  (lambda_r / (2 * n_samples) * np.sum(np.power(theta[1:], 2)))
 
-        grad = np.dot((1/n_samples) * X.T, hX - y)
-        theta[0, 0] = 0  # FIXME: this can be used in one offs but not items already fitted
-        grad = grad + np.dot((lambda_r/n_samples), theta)
-        grad = grad[:]
+        mask = np.ones((np.size(theta), 1))
+        mask[0] = 0
+        grad = np.divide(1, n_samples) * np.dot(X.T, (hX - y)) + lambda_r * (theta * mask) / n_samples
 
         return j_cost, grad
 
@@ -255,7 +257,7 @@ class Logistic(LinearMixin):
                                                   self.theta_, alpha=alpha,
                                                   max_iter=iterations,
                                                   lambda_r=lambda_r)
-            self.theta2_, self.grad_ = ls.gradient_descent(X, y, self.theta_, linearclass=self, alpha=alpha, max_iter=iterations)
+            #self.theta2_, self.grad_ = ls.gradient_descent(X, y, self.theta_, linearclass=self, alpha=alpha, max_iter=iterations)
 
     def predict(self, X, sum=False):
         """
@@ -263,6 +265,11 @@ class Logistic(LinearMixin):
         :param X:
         :return:
         """
+
+        # FIXME: Need to handle how we insert the bias intercept better
+        # FIXME: if X_data and y_data present they already have a column of 1's
+        # FIXME: we need a way to check for the bias column and if it does not exist
+        # FIXME: insert
 
         if not hasattr(self, 'theta_'):
             raise RuntimeError("Instance is currently not fitted")
@@ -283,7 +290,8 @@ class Logistic(LinearMixin):
         else:
             if sum:
                 # hypothesis in logistic model: h_theta(x) = theta_zero + theta_one * x_one
-                return np.sum(sigmoid.sigmoid(np.dot(X, self.theta_[:X.shape[0]])).astype(np.float32))
+                return np.sum(self._hypothesize(X, self.theta_[:X.shape[0]])).astype(np.float32)
+                #return np.sum(sigmoid.sigmoid(np.dot(X, self.theta_[:X.shape[0]])).astype(np.float32))
             else:
-                # p = sigmoid(X * theta) > sigmoid(0);
-                return sigmoid.sigmoid(np.dot(X, self.theta_[:X.shape[0]])) > sigmoid.sigmoid(0)
+                return (self._hypothesize(X, self.theta_) > sigmoid.sigmoid(0)).astype(np.int)
+                #return (sigmoid.sigmoid(np.dot(X, self.theta_)) > sigmoid.sigmoid(0)).astype(np.int)

pml/pmldemo.py
@@ -38,7 +38,7 @@ def demo_newlogistic():
     X, y = data.load_data(source='samples/sample_data3.txt', columns=3, features=[1,2])
     logistic_model = linear.Logistic(solver='logistic', normalize=False, max_iter=400)
     logistic_model.train(X, y)
-    result = logistic_model.predict([45, 85])
+    result = logistic_model.predict([45, 85], sum=True)
     print("For a student with scores 45 and 85, we predict an admission probability of {0:.2f}%".format(result*100))
 
 def demo_logisticreg():
@@ -49,8 +49,8 @@ def demo_logisticreg():
     # map polynomial features
     preprocessor = preprocessing.PolyFeatures(degrees=6)
     X = preprocessor.mapfeatures(X)
-    logistic_model.train(X, y)
-    pred = logistic_model.predict([2, 3])
+    logistic_model.train(X, y, lambda_r=1, iterations=400)
+    pred = logistic_model.predict(logistic_model.X_data)
     print('Train Accuracy: {}'.format(np.mean(np.double(pred == y)) * 100))
 
 def demo_multiclass():
@@ -59,10 +59,11 @@ def demo_multiclass():
     X, y = data.load_matdata(file_name='samples/ex3data1.mat')
     logistic_model = linear.Logistic(solver='logistic', normalize=False, num_of_labels=10, max_iter=50, lambda_r=3)
 
+    y = preprocessing.PolyFeatures.zeroalign(y) # <---- original data mapped based on 1-based index not 0 based
     logistic_model.train(X, y, lambda_r=0.1)
 
     pred = logistic_model.predict(X)
-    print('Training Set Accuracy: {}%'.format(np.mean(np.double(pred == y)) * 10000))
+    print('Training Set Accuracy: {}%'.format(np.mean(np.double(pred == y)) * 100))
 
     # FIXME: Need to have a common way of calling into cost calc and fit and do the right thing
     # should this be class vars or should they be defined at the methods themselves
@@ -79,10 +80,10 @@ if __name__ == '__main__':
     """Main function for console application"""
 
     # plot classification data
-    #demo_preprocessing()
-    #demo_newmultilinear()
-    #demo_newlinear()
-    #demo_newlogistic()
+    demo_preprocessing()
+    demo_newmultilinear()
+    demo_newlinear()
+    demo_newlogistic()
     demo_logisticreg()
     demo_multiclass()
 

pml/solvers/__init__.py
@@ -245,7 +245,7 @@ def fminfunc(costfunc, X, y, initial_theta, **kwargs):
         macheps = np.spacing(np.double(1))
 
     # initialize cost
-    cost_outer, _ = costfunc(X, y, initial_theta)
+    cost_outer, _ = costfunc(X, y, initial_theta, lambda_r)
 
     # outer loop of convergence
     while(n_iter < max_iter and n_funcval < max_evals and not info):
@@ -257,7 +257,7 @@ def fminfunc(costfunc, X, y, initial_theta, **kwargs):
         grad_prev = grad
 
         # call cost function and set gradient value
-        cost_outer, grad = costfunc(X, y, initial_theta)
+        cost_outer, grad = costfunc(X, y, theta_cpy, lambda_r)
         grad = grad[:]
         n_funcval += 1
 
@@ -299,7 +299,7 @@ def fminfunc(costfunc, X, y, initial_theta, **kwargs):
                 delta = min(delta, s_norm)
 
             # call cost function and evaluate cost based on initial cost in outer loop
-            cost_inner = costfunc(X, y, theta_cpy + s)[0]
+            cost_inner = costfunc(X, y, theta_cpy + s, lambda_r)[0]
 
             if cost_inner < cost_outer:  # scaled actual reduction (average)
                 actred = (cost_outer - cost_inner) / (abs(cost_inner) + abs(cost_outer))